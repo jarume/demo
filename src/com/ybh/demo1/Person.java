59061a062197c62baddb82d26709fbd2fd8c2245
yubanghua <yubanghua@sunline.cn>
2019-03-26 18:08:09 +0800

    提交

--- src/com/ybh/demo1/Person.java
+++ src/com/ybh/demo1/Person.java
@@ -6,7 +6,7 @@ public class Person {
 	String add = "杭州";
 	public static void main(String[] args) {
 		System.out.println("1");
-		
+		System.out.println("2");
 		System.out.println("ppppppppp")；
 	}
 }
