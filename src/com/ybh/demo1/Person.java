f4dabe0b14c71479984e88e91fcd7b52312cff99
yubanghua <yubanghua@sunline.cn>
2019-03-26 16:16:49 +0800

    提交代码

--- src/com/ybh/demo1/Person.java
+++ src/com/ybh/demo1/Person.java
@@ -5,7 +5,7 @@ public class Person {
 	String name = "张三";
 	String add = "杭州";
 	public static void main(String[] args) {
-		System.out.println();
+		System.out.println("1");
 		System.out.println();
 		System.out.println();
 	}
