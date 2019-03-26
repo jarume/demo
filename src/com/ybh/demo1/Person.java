4a664238a35239446670b28cfd46dae19660c14e
ybh <baodecaiying@qq.com>
2019-03-26 17:52:58 +0800

    是是是

--- src/com/ybh/demo1/Person.java
+++ src/com/ybh/demo1/Person.java
@@ -2,11 +2,11 @@
 
 public class Person {
 
-	String name = "张三";
+	String name = "张三ss";
 	String add = "杭州";
 	public static void main(String[] args) {
 		System.out.println("1");
-		System.out.println();
-		System.out.println();
+		
+		System.out.println("ppppppppp");
 	}
 }
