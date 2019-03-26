e00d962f7c273c0200e94a9668b7f47ea7fd8e39
ybh <baodecaiying@qq.com>
2019-03-26 15:56:23 +0800

    ybh提交打印 Signed-off-by: ybh <baodecaiying@qq.com>

--- src/com/ybh/demo1/Person.java
+++ src/com/ybh/demo1/Person.java
@@ -1,10 +1,10 @@
-package com.ybh.demo1;
+﻿package com.ybh.demo1;
 
 public class Person {
 
 	String name = "张三";
 	String add = "杭州";
 	public static void main(String[] args) {
-		
+	       System.out.println();
 	}
 }
